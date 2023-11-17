f420df5df8d1d17efdde095ad8c7d49b54d87e6b
maksyuki <maksyuki@126.com>
2023-11-17 21:31:10 +0800

    feat: rewrite all module by using reg and edge det modules

--- rtl/apb4_gpio.sv
+++ rtl/apb4_gpio.sv
@@ -20,6 +20,8 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
+`include "register.sv"
+`include "edge_det.sv"
 `include "gpio_define.sv"
 
 module apb4_gpio #(
@@ -31,24 +33,19 @@ module apb4_gpio #(
 
   logic [3:0] s_apb4_addr;
   logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
-  logic [GPIO_NUM-1:0] r_gpio_inten;
-  logic [GPIO_NUM-1:0] r_gpio_inttype0;
-  logic [GPIO_NUM-1:0] r_gpio_inttype1;
-  logic [GPIO_NUM-1:0] r_gpio_out;
-  logic [GPIO_NUM-1:0] r_gpio_dir;
-  logic [GPIO_NUM-1:0] r_gpio_sync0;
-  logic [GPIO_NUM-1:0] r_gpio_sync1;
-  logic [GPIO_NUM-1:0] r_gpio_in;
-  logic [GPIO_NUM-1:0] r_iofcfg;
-  logic [GPIO_NUM-1:0] r_status;
-  logic [GPIO_NUM-1:0] s_gpio_rise;
-  logic [GPIO_NUM-1:0] s_gpio_fall;
-  logic [GPIO_NUM-1:0] s_is_int_rise;
-  logic [GPIO_NUM-1:0] s_is_int_fall;
-  logic [GPIO_NUM-1:0] s_is_int_lev0;
-  logic [GPIO_NUM-1:0] s_is_int_lev1;
-  logic [GPIO_NUM-1:0] s_is_int_all;
-  logic                s_rise_int;
+  logic [GPIO_NUM-1:0] s_gpio_inten_d, s_gpio_inten_q;
+  logic [GPIO_NUM-1:0] s_gpio_inttype0_d, s_gpio_inttype0_q;
+  logic [GPIO_NUM-1:0] s_gpio_inttype1_d, s_gpio_inttype1_q;
+  logic [GPIO_NUM-1:0] s_gpio_out_d, s_gpio_out_q;
+  logic [GPIO_NUM-1:0] s_gpio_dir_d, s_gpio_dir_q;
+  logic [GPIO_NUM-1:0] s_gpio_in;
+  logic [GPIO_NUM-1:0] s_gpio_iofcfg_d, s_gpio_iofcfg_q;
+  logic [GPIO_NUM-1:0] s_gpio_intstatus_d, s_gpio_intstatus_q;
+  logic [GPIO_NUM-1:0] r_gpio_sync0, r_gpio_sync1;
+  logic [GPIO_NUM-1:0] s_gpio_rise, s_gpio_fall;
+  logic [GPIO_NUM-1:0] s_is_int_rise, s_is_int_fall;
+  logic [GPIO_NUM-1:0] s_is_int_lev0, s_is_int_lev1, s_is_int_all;
+  logic s_irq_d, s_irq_q, s_rise_int;
 
   assign s_apb4_addr = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
@@ -56,79 +53,117 @@ module apb4_gpio #(
   assign apb4.pready = 1'b1;
   assign apb4.pslverr = 1'b0;
 
-  assign gpio.gpio_iof_o = r_iofcfg;
-  assign gpio.gpio_out_o = r_gpio_out;
-  assign gpio.gpio_dir_o = r_gpio_dir;
-  assign s_gpio_rise = r_gpio_sync1 & ~r_gpio_in;  // check if rising edge
-  assign s_gpio_fall = ~r_gpio_sync1 & r_gpio_in;  // check if falling edge
+  assign gpio.gpio_dir_o = s_gpio_dir_q;
+  assign gpio.gpio_out_o = s_gpio_out_q;
+  assign gpio.gpio_iof_o = s_gpio_iofcfg_q;
+  assign gpio.irq_o = s_irq_q;
 
-  assign s_is_int_rise = (r_gpio_inttype1 & ~r_gpio_inttype0) & s_gpio_rise;
-  assign s_is_int_fall = (r_gpio_inttype1 & r_gpio_inttype0) & s_gpio_fall;
-  assign s_is_int_lev0 = (~r_gpio_inttype1 & r_gpio_inttype0) & ~r_gpio_in;
-  assign s_is_int_lev1 = (~r_gpio_inttype1 & ~r_gpio_inttype0) & r_gpio_in;
-
-  // check if bit if gpio.irq_o is enable and if gpio.irq_o specified by inttype occurred
-  assign s_is_int_all  = r_gpio_inten & (s_is_int_rise | s_is_int_fall | s_is_int_lev0 | s_is_int_lev1);
+  assign s_is_int_rise = (s_gpio_inttype1_q & ~s_gpio_inttype0_q) & s_gpio_rise;
+  assign s_is_int_fall = (s_gpio_inttype1_q & s_gpio_inttype0_q) & s_gpio_fall;
+  assign s_is_int_lev0 = (~s_gpio_inttype1_q & s_gpio_inttype0_q) & ~s_gpio_in;
+  assign s_is_int_lev1 = (~s_gpio_inttype1_q & ~s_gpio_inttype0_q) & s_gpio_in;
+  assign s_is_int_all  = s_gpio_inten_q & (s_is_int_rise | s_is_int_fall | s_is_int_lev0 | s_is_int_lev1);
   assign s_rise_int = |s_is_int_all;
 
-  always_ff @(posedge apb4.pclk, negedge apb4.presetn) begin
-    if (~apb4.presetn) begin
-      gpio.irq_o <= 1'b0;
-      r_status   <= '0;
-    end else if (~gpio.irq_o && s_rise_int) begin  // rise gpio.irq_o if not already rise
-      gpio.irq_o <= 1'b1;
-      r_status   <= s_is_int_all;
-    end else if (gpio.irq_o && s_apb4_rd_hdshk && (s_apb4_addr == `GPIO_INTSTATUS)) begin //clears int if status is read
-      gpio.irq_o <= 1'b0;
-      r_status   <= '0;
-    end
-  end
+  edge_det #(2, GPIO_NUM) u_edge_det (
+      apb4.pclk,
+      apb4.presetn,
+      gpio.gpio_in_i,
+      s_gpio_in,
+      s_gpio_rise,
+      s_gpio_fall
+  );
 
-  // first 2 sync for metastability resolving, last reg used for edge detection
-  always_ff @(posedge apb4.pclk, negedge apb4.presetn) begin
-    if (~apb4.presetn) begin
-      r_gpio_sync0 <= '0;
-      r_gpio_sync1 <= '0;
-      r_gpio_in    <= '0;
-    end else begin
-      r_gpio_sync0 <= gpio.gpio_in_i;
-      r_gpio_sync1 <= r_gpio_sync0;
-      r_gpio_in    <= r_gpio_sync1;
-    end
-  end
+  assign s_gpio_dir_d = (s_apb4_wr_hdshk && s_apb4_addr == `GPIO_PADDIR) ? apb4.pwdata[GPIO_NUM-1:0] : s_gpio_dir_q;
+  dffr #(GPIO_NUM) u_gpio_paddir_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_gpio_dir_d,
+      s_gpio_dir_q
+  );
 
-  always_ff @(posedge apb4.pclk, negedge apb4.presetn) begin
-    if (~apb4.presetn) begin
-      r_gpio_inten    <= '0;
-      r_gpio_inttype0 <= '0;
-      r_gpio_inttype1 <= '0;
-      r_gpio_out      <= '0;
-      r_gpio_dir      <= '0;
-      r_iofcfg        <= '0;
-    end else if (s_apb4_wr_hdshk) begin
-      unique case (s_apb4_addr)
-        `GPIO_PADDIR:   r_gpio_dir <= apb4.pwdata[GPIO_NUM-1:0];
-        `GPIO_PADOUT:   r_gpio_out <= apb4.pwdata[GPIO_NUM-1:0];
-        `GPIO_INTEN:    r_gpio_inten <= apb4.pwdata[GPIO_NUM-1:0];
-        `GPIO_INTTYPE0: r_gpio_inttype0 <= apb4.pwdata[GPIO_NUM-1:0];
-        `GPIO_INTTYPE1: r_gpio_inttype1 <= apb4.pwdata[GPIO_NUM-1:0];
-        `GPIO_IOFCFG:   r_iofcfg <= apb4.pwdata[GPIO_NUM-1:0];
-      endcase
+  assign s_gpio_out_d = (s_apb4_wr_hdshk && s_apb4_addr == `GPIO_PADOUT) ? apb4.pwdata[GPIO_NUM-1:0] : s_gpio_out_q;
+  dffr #(GPIO_NUM) u_gpio_padin_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_gpio_out_d,
+      s_gpio_out_q
+  );
+
+  assign s_gpio_inten_d = (s_apb4_wr_hdshk && s_apb4_addr == `GPIO_INTEN) ? apb4.pwdata[GPIO_NUM-1:0] : s_gpio_inten_q;
+  dffr #(GPIO_NUM) u_gpio_inten_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_gpio_inten_d,
+      s_gpio_inten_q
+  );
+
+  assign s_gpio_inttype0_d = (s_apb4_wr_hdshk && s_apb4_addr == `GPIO_INTTYPE0) ? apb4.pwdata[GPIO_NUM-1:0] : s_gpio_inttype0_q;
+  dffr #(GPIO_NUM) u_gpio_inttype0_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_gpio_inttype0_d,
+      s_gpio_inttype0_q
+  );
+
+  assign s_gpio_inttype1_d = (s_apb4_wr_hdshk && s_apb4_addr == `GPIO_INTTYPE1) ? apb4.pwdata[GPIO_NUM-1:0] : s_gpio_inttype1_q;
+  dffr #(GPIO_NUM) u_gpio_inttype1_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_gpio_inttype1_d,
+      s_gpio_inttype1_q
+  );
+
+  assign s_gpio_iofcfg_d = (s_apb4_wr_hdshk && s_apb4_addr == `GPIO_IOFCFG) ? apb4.pwdata[GPIO_NUM-1:0] : s_gpio_iofcfg_q;
+  dffr #(GPIO_NUM) u_gpio_iofcfg_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_gpio_iofcfg_d,
+      s_gpio_iofcfg_q
+  );
+
+  always_comb begin
+    s_irq_d = s_irq_q;
+    if (~s_irq_q && s_rise_int) begin
+      s_irq_d = 1'b1;
+    end else if (s_irq_q && s_apb4_rd_hdshk && s_apb4_addr == `GPIO_INTSTATUS) begin
+      s_irq_d = 1'b0;
     end
   end
+  dffr #(1) u_irq_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_irq_d,
+      s_irq_q
+  );
+
+  always_comb begin
+    s_gpio_intstatus_d = s_gpio_intstatus_q;
+    if (~s_irq_q && s_rise_int) begin
+      s_gpio_intstatus_d = s_is_int_all;
+    end else if (s_irq_q && s_apb4_rd_hdshk && s_apb4_addr == `GPIO_INTSTATUS) begin
+      s_gpio_intstatus_d = '0;
+    end
+  end
+  dffr #(GPIO_NUM) u_gpio_intstatus_dffr (
+      apb4.pclk,
+      apb4.presetn,
+      s_gpio_intstatus_d,
+      s_gpio_intstatus_q
+  );
 
   always_comb begin
     apb4.prdata = '0;
     if (s_apb4_rd_hdshk) begin
       unique case (s_apb4_addr)
-        `GPIO_PADDIR:    apb4.prdata[GPIO_NUM-1:0] = r_gpio_dir;
-        `GPIO_PADIN:     apb4.prdata[GPIO_NUM-1:0] = r_gpio_in;
-        `GPIO_PADOUT:    apb4.prdata[GPIO_NUM-1:0] = r_gpio_out;
-        `GPIO_INTEN:     apb4.prdata[GPIO_NUM-1:0] = r_gpio_inten;
-        `GPIO_INTTYPE0:  apb4.prdata[GPIO_NUM-1:0] = r_gpio_inttype0;
-        `GPIO_INTTYPE1:  apb4.prdata[GPIO_NUM-1:0] = r_gpio_inttype1;
-        `GPIO_INTSTATUS: apb4.prdata[GPIO_NUM-1:0] = r_status;
-        `GPIO_IOFCFG:    apb4.prdata[GPIO_NUM-1:0] = r_iofcfg;
+        `GPIO_PADDIR:    apb4.prdata[GPIO_NUM-1:0] = s_gpio_dir_q;
+        `GPIO_PADIN:     apb4.prdata[GPIO_NUM-1:0] = s_gpio_in;
+        `GPIO_PADOUT:    apb4.prdata[GPIO_NUM-1:0] = s_gpio_out_q;
+        `GPIO_INTEN:     apb4.prdata[GPIO_NUM-1:0] = s_gpio_inten_q;
+        `GPIO_INTTYPE0:  apb4.prdata[GPIO_NUM-1:0] = s_gpio_inttype0_q;
+        `GPIO_INTTYPE1:  apb4.prdata[GPIO_NUM-1:0] = s_gpio_inttype1_q;
+        `GPIO_INTSTATUS: apb4.prdata[GPIO_NUM-1:0] = s_gpio_intstatus_q;
+        `GPIO_IOFCFG:    apb4.prdata[GPIO_NUM-1:0] = s_gpio_iofcfg_q;
         default:         apb4.prdata[GPIO_NUM-1:0] = '0;
       endcase
     end
