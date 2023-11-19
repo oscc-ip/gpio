a7026a3b7d9e4d9f5b3da7b4a24d2fe15e778272
maksyuki <maksyuki@126.com>
2023-11-19 15:51:16 +0800

    feat: modify irq test task impl

--- tb/gpio_test.sv
+++ tb/gpio_test.sv
@@ -16,20 +16,25 @@
 
 class GPIOTest extends APB4Master;
   string                 name;
-  int                    gpio_num,     gpio_mask;
-  int                    dir_val,      in_val;
-  int                    out_val,      inten_val;
-  int                    inttype0_val, inttype1_val;
+  int                    gpio_num;
+  int                    gpio_mask;
+  int                    wr_val;
+  int                    dir_val;
+  int                    in_val;
+  int                    out_val;
+  int                    inten_val;
+  int                    inttype0_val;
+  int                    inttype1_val;
   virtual apb4_if.master apb4;
   virtual gpio_if.tb     gpio;
 
   extern function new(string name = "gpio_test", int gpio_num, virtual apb4_if.master apb4,
                       virtual gpio_if.tb gpio);
-  extern task test_reset_reg();
-  extern task test_wr_rd_reg(input bit [31:0] run_times = 1000);
-  extern task test_gpio_io(input bit [31:0] run_times = 1000);
-  extern task test_gpio_cfg(input bit [31:0] run_times = 1000);
-  extern task test_irq(input bit [31:0] run_times = 1000);
+  extern task automatic test_reset_reg();
+  extern task automatic test_wr_rd_reg(input bit [31:0] run_times = 1000);
+  extern task automatic test_gpio_io(input bit [31:0] run_times = 1000);
+  extern task automatic test_gpio_cfg(input bit [31:0] run_times = 1000);
+  extern task automatic test_irq(input bit [31:0] run_times = 1000);
 endclass
 
 function GPIOTest::new(string name, int gpio_num, virtual apb4_if.master apb4,
@@ -38,6 +43,7 @@ function GPIOTest::new(string name, int gpio_num, virtual apb4_if.master apb4,
   this.name         = name;
   this.gpio_num     = gpio_num;
   this.gpio_mask    = (1 << (gpio_num)) - 1;
+  this.wr_val       = 0;
   this.dir_val      = 0;
   this.in_val       = 0;
   this.out_val      = 0;
@@ -48,7 +54,7 @@ function GPIOTest::new(string name, int gpio_num, virtual apb4_if.master apb4,
   this.gpio         = gpio;
 endfunction
 
-task GPIOTest::test_reset_reg();
+task automatic GPIOTest::test_reset_reg();
   super.test_reset_reg();
   // verilog_format: off
   this.rd_check(`GPIO_PADDIR_ADDR, "PADDIR REG", 32'b0 & this.gpio_mask, Helper::EQUL, Helper::INFO);
@@ -62,21 +68,21 @@ task GPIOTest::test_reset_reg();
   // verilog_format: on
 endtask
 
-task GPIOTest::test_wr_rd_reg(input bit [31:0] run_times = 1000);
+task automatic GPIOTest::test_wr_rd_reg(input bit [31:0] run_times = 1000);
   super.test_wr_rd_reg();
   // verilog_format: off
   for (int i = 0; i < run_times; i++) begin
-    this.wr_check(`GPIO_PADDIR_ADDR, "PADDIR REG", $random & this.gpio_mask, Helper::EQUL);
-    this.wr_check(`GPIO_PADOUT_ADDR, "PADOUT REG", $random & this.gpio_mask, Helper::EQUL);
-    this.wr_check(`GPIO_INTEN_ADDR, "INTEN REG", $random & this.gpio_mask, Helper::EQUL);
-    this.wr_check(`GPIO_INTTYPE0_ADDR, "INTTYPE0 REG", $random & this.gpio_mask, Helper::EQUL);
-    this.wr_check(`GPIO_INTTYPE1_ADDR, "INTTYPE1 REG", $random & this.gpio_mask, Helper::EQUL);
-    this.wr_check(`GPIO_IOFCFG_ADDR, "IOFCFG REG", $random & this.gpio_mask, Helper::EQUL);
+    this.wr_rd_check(`GPIO_PADDIR_ADDR, "PADDIR REG", $random & this.gpio_mask, Helper::EQUL);
+    this.wr_rd_check(`GPIO_PADOUT_ADDR, "PADOUT REG", $random & this.gpio_mask, Helper::EQUL);
+    this.wr_rd_check(`GPIO_INTEN_ADDR, "INTEN REG", $random & this.gpio_mask, Helper::EQUL);
+    this.wr_rd_check(`GPIO_INTTYPE0_ADDR, "INTTYPE0 REG", $random & this.gpio_mask, Helper::EQUL);
+    this.wr_rd_check(`GPIO_INTTYPE1_ADDR, "INTTYPE1 REG", $random & this.gpio_mask, Helper::EQUL);
+    this.wr_rd_check(`GPIO_IOFCFG_ADDR, "IOFCFG REG", $random & this.gpio_mask, Helper::EQUL);
   end
   // verilog_format: on
 endtask
 
-task GPIOTest::test_gpio_io(input bit [31:0] run_times = 1000);
+task automatic GPIOTest::test_gpio_io(input bit [31:0] run_times = 1000);
   $display("=== [test gpio io] ===");
   // reg: dir, out, in, clear int enable
   this.write(`GPIO_INTEN_ADDR, 32'b0);
@@ -86,47 +92,98 @@ task GPIOTest::test_gpio_io(input bit [31:0] run_times = 1000);
     this.out_val = $random & this.gpio_mask;
 
     this.write(`GPIO_PADDIR_ADDR, this.dir_val);
-    this.gpio.gpio_in_i = this.in_val;
+    Helper::check("GPIO DIR", this.dir_val, this.gpio.gpio_dir_o, Helper::EQUL);
     this.write(`GPIO_PADOUT_ADDR, this.out_val);
-
     Helper::check("GPIO WRITE OUT", this.gpio.gpio_out_o, this.out_val, Helper::EQUL);
-    Helper::check("GPIO DIR", this.gpio.gpio_dir_o, this.dir_val, Helper::EQUL);
+    this.gpio.gpio_in_i = this.in_val;
+    repeat (4) @(posedge apb4.pclk);
+    #1;
     this.rd_check(`GPIO_PADIN_ADDR, "GPIO READ IN", this.in_val, Helper::EQUL);
   end
 endtask
 
-task GPIOTest::test_gpio_cfg(input bit [31:0] run_times = 1000);
+task automatic GPIOTest::test_gpio_cfg(input bit [31:0] run_times = 1000);
   $display("=== [test gpio cfg] ===");
   for (int i = 0; i < run_times; i++) begin
-    this.wr_check(`GPIO_IOFCFG_ADDR, "IOFCFG REG", $random & this.gpio_mask, Helper::EQUL);
+    this.wr_val = $random & this.gpio_mask;
+    this.write(`GPIO_IOFCFG_ADDR, this.wr_val);
+    Helper::check("IOFCFG REG", this.wr_val, this.gpio.gpio_iof_o, Helper::EQUL);
   end
 endtask
 
-task GPIOTest::test_irq(input bit [31:0] run_times = 1000);
+task automatic GPIOTest::test_irq(input bit [31:0] run_times = 1000);
   super.test_irq();
-  // reg: inten, type0, type1
-  // check irq, status
+  // all irq high triggered test
+  for (int i = 0; i < run_times; i++) begin
+    this.in_val         = $random & this.gpio_mask;
+    // NOTE: need to set inttype first
+    this.gpio.gpio_in_i = '0;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    repeat (6) @(posedge this.apb4.pclk);  // NOTE: for edge det
+    #1;
+    this.write(`GPIO_INTTYPE0_ADDR, '0);
+    this.write(`GPIO_INTTYPE1_ADDR, '0);
+    this.write(`GPIO_INTEN_ADDR, '1);
+
+    this.gpio.gpio_in_i = this.in_val;
+    repeat (6) @(posedge this.apb4.pclk);
+    #1;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    this.rd_check(`GPIO_INTSTATUS_ADDR, "INTSTATUS REG", this.in_val, Helper::EQUL);
+  end
+
+  // all irq low triggered test
   for (int i = 0; i < run_times; i++) begin
     this.in_val = $random & this.gpio_mask;
-    // this.in_val = 8'hFF;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    this.gpio.gpio_in_i = '1;
+    repeat (6) @(posedge this.apb4.pclk);
+    #1;
+    this.write(`GPIO_INTTYPE0_ADDR, '1);
+    this.write(`GPIO_INTTYPE1_ADDR, '0);
+    this.write(`GPIO_INTEN_ADDR, '1);
 
-    $display("1: in_val: %h, super.rd_data: %h", this.in_val, super.rd_data);
-    // all irq high triggered test
-    this.write(`GPIO_INTEN_ADDR, 32'hFF);
-    this.write(`GPIO_INTTYPE0_ADDR, 32'b0);
-    this.write(`GPIO_INTTYPE1_ADDR, 32'b0);
+    this.gpio.gpio_in_i = ~this.in_val;
+    repeat (6) @(posedge this.apb4.pclk);
+    #1;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    this.rd_check(`GPIO_INTSTATUS_ADDR, "INTSTATUS REG", this.in_val, Helper::EQUL);
+  end
+
+  // all irq rise triggered test
+  for (int i = 0; i < run_times; i++) begin
+    this.in_val         = $random & this.gpio_mask;
+    this.gpio.gpio_in_i = '0;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    repeat (6) @(posedge this.apb4.pclk);
+    #1;
+    this.write(`GPIO_INTTYPE0_ADDR, '0);
+    this.write(`GPIO_INTTYPE1_ADDR, '1);
+    this.write(`GPIO_INTEN_ADDR, '1);
+
     this.gpio.gpio_in_i = this.in_val;
-    repeat (4) @(posedge this.apb4.pclk);
-    this.read(`GPIO_INTSTATUS_ADDR);
-    $display("2: in_val: %h, super.rd_data: %h", this.in_val, super.rd_data);
-    this.read(`GPIO_INTSTATUS_ADDR);
-    $display("3: in_val: %h, super.rd_data: %h", this.in_val, super.rd_data);
-    // this.rd_check(`GPIO_INTSTATUS_ADDR, "INTSTATUS REG", 32'hFF, Helper::EQUL);
+    repeat (6) @(posedge this.apb4.pclk);
+    #1;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    this.rd_check(`GPIO_INTSTATUS_ADDR, "INTSTATUS REG", this.in_val, Helper::EQUL);
+  end
 
+  // all irq fall triggered test
+  for (int i = 0; i < run_times; i++) begin
+    this.in_val         = $random & this.gpio_mask;
+    this.gpio.gpio_in_i = '1;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    repeat (6) @(posedge this.apb4.pclk);
+    #1;
+    this.write(`GPIO_INTTYPE0_ADDR, '1);
+    this.write(`GPIO_INTTYPE1_ADDR, '1);
+    this.write(`GPIO_INTEN_ADDR, '1);
 
-    // this.inten_val    = $random & this.gpio_mask;
-    // this.inttype0_val = $random & this.gpio_mask;
-    // this.inttype1_val = $random & this.gpio_mask;
+    this.gpio.gpio_in_i = ~this.in_val;
+    repeat (6) @(posedge this.apb4.pclk);
+    #1;
+    this.write(`GPIO_INTEN_ADDR, '0);
+    this.rd_check(`GPIO_INTSTATUS_ADDR, "INTSTATUS REG", this.in_val, Helper::EQUL);
   end
 endtask
 
